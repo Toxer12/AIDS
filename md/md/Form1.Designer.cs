b043970f9335c5fd27cd027704fe038f5baaf18b
Toxer12 <115220195+Toxer12@users.noreply.github.com>
2023-10-18 10:47:19 +0200

    Add files via upload

---
+++ md/md/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace md
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(384, 307);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Start";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ md/md/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+namespace md
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            int[] tab = { 7, 2, 5, 4, 1 };
+
+            MessageBox.Show(toString(babel(tab)).ToString());
+        }
+
+        int[] babel(int[] tab)
+        {
+            bool a = false;
+            int t;
+            
+            for(int i =0; i < tab.Length-1; i++)
+            {
+                if (tab[i] > tab[i+1])
+                {
+                    t = tab[i];
+                    tab[i] = tab[i + 1];
+                    tab[i + 1] = t;
+                    a = true;
+                }
+            }
+
+            if(a==true)
+            {
+                a = false;
+                return babel(tab);
+            }
+            else
+            {
+                return tab;
+            }
+
+        }
+
+        string toString(int[] tab1)
+        {
+            string wynik = "";
+            for (int i = 0; i < tab1.Length; i++)
+            {
+                wynik += tab1[i] + ", ";
+            }
+            return wynik;
+        }
+    }
+}
